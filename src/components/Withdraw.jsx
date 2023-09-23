d6b1554ec7664706384f17333c9141d02966ab34
toney010319 <toney010319@gmail.com>
2023-09-24 03:59:21 +0800

    Adding Validation of inputs  and setError in Send Component

--- src/components/Withdraw.jsx
+++ src/components/Withdraw.jsx
@@ -4,7 +4,7 @@ import TransactionHistory from "./TransactionHistory";
 const Withdraw = (props) => {
   const { user, setUser } = props;
   const [formValue, setFormValue] = useState("");
-
+  const [errors, setErrors] = useState({});
   const date = new Date();
   const formattedDate = date.toLocaleDateString("en-US", {
     year: "numeric",
@@ -16,34 +16,51 @@ const Withdraw = (props) => {
   const onChange = (e) => {
     setFormValue(e.target.value);
   };
+  const Validate = () => {
+    const balanceValidation = user.balance >= formValue;
+    const amountValidation = formValue <= 0;
+    const validationErrors = {};
 
+    if (!balanceValidation) {
+      validationErrors.amount = "insuficient Balance.";
+    }
+
+    if (amountValidation)
+      validationErrors.isnumber = "Please Enter Valid Amount";
+
+    return validationErrors;
+  };
   const onSubmit = (event) => {
     event.preventDefault();
     let newAccountDetails = {};
-    const storedAccounts = JSON.parse(localStorage.getItem("Accounts"));
-    const updatedAccounts = storedAccounts.map((account) => {
-      if (account.email === user.email) {
-        const newBalance = (account.balance -= parseFloat(formValue));
-        const newTransaction = {
-          type: "Witdhdraw",
-          amount: `$${formValue}.00`,
-          time: formattedDate,
-        };
-        account.transaction.push(newTransaction);
-        // user.balance = newBalance
-        // user.transaction = account.transaction
-        newAccountDetails = {
-          ...account,
-          balance: newBalance,
-          transaction: [...(account.transaction || [])],
-        };
-      }
+    const validationErrors = Validate(formValue);
+    if (Object.keys(validationErrors).length === 0) {
+      const storedAccounts = JSON.parse(localStorage.getItem("Accounts"));
+      const updatedAccounts = storedAccounts.map((account) => {
+        if (account.email === user.email) {
+          const newBalance = (account.balance -= parseFloat(formValue));
+          const newTransaction = {
+            type: "Witdhdraw",
+            amount: `$${formValue}.00`,
+            time: formattedDate,
+          };
+          account.transaction.push(newTransaction);
+          // user.balance = newBalance
+          // user.transaction = account.transaction
+          newAccountDetails = {
+            ...account,
+            balance: newBalance,
+            transaction: [...(account.transaction || [])],
+          };
+        }
 
-      return account;
-    });
-
-    localStorage.setItem("Accounts", JSON.stringify(updatedAccounts));
-    setUser(newAccountDetails);
+        return account;
+      });
+      alert(" Successfully Withdraw ");
+      localStorage.setItem("Accounts", JSON.stringify(updatedAccounts));
+      setUser(newAccountDetails);
+    }
+    setErrors(validationErrors);
   };
 
   return (
@@ -57,6 +74,8 @@ const Withdraw = (props) => {
           placeholder="$0.00"
           onChange={onChange}
         />
+        {errors.amount && <div>{errors.amount}</div>}
+        {errors.isnumber && <div>{errors.isnumber}</div>}
 
         <button type="submit">Witdraw</button>
       </form>
