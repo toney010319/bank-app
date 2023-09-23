225eb7d8510ff558aff1efaf648b4c9445804e3d
toney010319 <toney010319@gmail.com>
2023-09-24 01:54:54 +0800

    Passing the state props setUser to the Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,40 +1,44 @@
-
-import AddExpense from "./Addexpense"
-import BudgetTracker from "./BudgetTracker"
-import TransactionHistory from "./TransactionHistory"
-
+import AddExpense from "./Addexpense";
+import BudgetTracker from "./BudgetTracker";
+import TransactionHistory from "./TransactionHistory";
 
 const Home = (props) => {
-    const { user, setSwitchPage } = props
+  const { user, setSwitchPage, setUser } = props;
 
-    return (
+  return (
+    <>
+      {user && (
         <>
-            {user && (
-                <>
-                    <div>
-                        <h1>Home</h1>
-                        <div>
-                            <p>Firstname: <b>{user.firstname}</b>  </p>
-                            <p>LASTNAME: <b>{user.lastname}</b></p>
-                            <p>EMAIL:<b>{user.email}</b></p>
+          <div>
+            <h1>Home</h1>
+            <div>
+              <p>
+                Firstname: <b>{user.firstname}</b>{" "}
+              </p>
+              <p>
+                LASTNAME: <b>{user.lastname}</b>
+              </p>
+              <p>
+                EMAIL:<b>{user.email}</b>
+              </p>
 
+              <p>
+                Balance: <b>${user.balance}.00</b>{" "}
+              </p>
+            </div>
+          </div>
+          <TransactionHistory user={user} />
 
-                            <p>Balance: <b>${user.balance}.00</b> </p>
-                        </div>
-                    </div>
-                    <TransactionHistory user={user} />
-
-                    <div>
-
-                        <BudgetTracker user={user} setSwitchPage={setSwitchPage} />
-                    </div>
-
-
-
-                </>
-            )}
+          <div>
+            <BudgetTracker
+              user={user}
+              setSwitchPage={setSwitchPage}
+              setUser={setUser}
+            />
+          </div>
         </>
-    )
-}
-export default Home
-
+      )}
+    </>
+  );
+};
+export default Home;
